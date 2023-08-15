88bb8f18008e4d716767d14bc71208641144889a
Loufa <72950179+Draggir@users.noreply.github.com>
2023-08-15 15:18:37 +0000

    Add files via upload
    start/first pushing

---
+++ script.js
@@ -0,0 +1,51 @@
+const searchIcon = document.querySelector('.navbar__search-icon');
+const searchContainer = document.querySelector('.navbar__search-container');
+
+let isSearchVisible = false; // Ajouter une variable pour suivre l'état de la barre de recherche
+
+searchIcon.addEventListener('click', () => {
+  if (isSearchVisible) {
+    searchContainer.classList.remove('visible');
+  } else {
+    searchContainer.classList.add('visible');
+  }
+  
+  isSearchVisible = !isSearchVisible; // Inverser l'état de la barre de recherche
+});
+
+const menuIconOpen = document.getElementById('menu-icon-open');
+const menuIconClose = document.getElementById('menu-icon-close');
+const menu = document.querySelector('.navbar__menu');
+
+menuIconOpen.addEventListener('click', () => {
+  menu.classList.add('visible');
+});
+
+menuIconClose.addEventListener('click', () => {
+  menu.classList.remove('visible');
+});
+
+var swiper = new Swiper(".Slider-1", {
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay:2000,
+    disableOnInteraction:false,
+  },
+  breakpoints: {
+    0:{
+      slidesPerView:1,
+    },
+    768:{
+      slidesPerView:2,
+    },
+    1024:{
+      slidesPerView:3,
+    },
+  }
+});
+
+var swiper = new Swiper(".trends-cards", {
+  effect: "cards",
+  grabCursor: true,
+});
